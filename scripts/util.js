03b88c62f5108d55a5701d481beef170d00ba199
Paul van Duyn <paul.vanduyn@gmail.com>
2018-12-10 13:49:39 -0500

    add box shadow, kukulkan gif, favicon

--- scripts/util.js
+++ scripts/util.js
@@ -2,10 +2,15 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const mutubePic = document.getElementById('mutube-pic');
+  const kukulkanPic = document.getElementById('kukulkan-pic');
 
   mutubePic.onmouseover = mutubeToGif;
   mutubePic.onmouseleave = mutubeToPng;
+  kukulkanPic.onmouseover = kukulkanToGif;
+  kukulkanPic.onmouseleave = kukulkanToPng;
 });
 
+const kukulkanToGif = (e) => e.target.setAttribute('src', './assets/kukulkan.gif');
+const kukulkanToPng = (e) => e.target.setAttribute('src', './assets/kukulkan.png');
 const mutubeToGif = (e) => e.target.setAttribute('src', './assets/mutube.gif');
 const mutubeToPng = (e) => e.target.setAttribute('src', './assets/mutube.png');
